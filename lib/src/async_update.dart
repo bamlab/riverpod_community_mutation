e875158b6b7abae3eb18b9659f86a6e5d837da5d
matthias-prevost <matthias.prevost@gmail.com>
2025-05-21 15:30:28 +0200

    fix: rewrite AsyncUpdate without freezed

--- lib/src/async_update.dart
+++ lib/src/async_update.dart
@@ -1,17 +1,9 @@
-// Importing foundation.dart also imports classes to make an object nicely
-// readable in Flutter's devtool.
-// If you import foundation.dart, Freezed will automatically do it for you.
-// ignore: unnecessary_import
-import 'package:freezed_annotation/freezed_annotation.dart';
-
-part 'async_update.freezed.dart';
-
 /// A utility for safely manipulating asynchronous mutation data.
 ///
 /// There are two differences with riverpod `AsyncData`:
 /// * The Update can be `Idle` when the mutation has not been executed.
-/// * The is no `refreshing` because we do not have a use case for it.
-sealed class AsyncUpdate<TData> with _$AsyncUpdate<TData> {
+/// * There is no `refreshing` because we do not have a use case for it.
+sealed class AsyncUpdate<TData> {
   const AsyncUpdate._();
 
   /// Your mutation has never run, `mutate` hasn't been called yet.
@@ -58,14 +50,113 @@ sealed class AsyncUpdate<TData> with _$AsyncUpdate<TData> {
       };
 
   /// Will return the error if status is error, otherwise null.
-  // ignore: no-object-declaration, ok for error
   Object? get error => switch (this) {
         ErrorDetails(:final error) => error,
         _ => null,
       };
+
+  /// Perform some action based on the current state of the [AsyncUpdate].
+  ///
+  /// This allows reading the content of an [AsyncUpdate] in a type-safe way,
+  /// without potentially ignoring to handle a case.
+  R map<R>({
+    required R Function() idle,
+    required R Function(TData data) data,
+    required R Function(Object? error, StackTrace? stackTrace) error,
+    required R Function() loading,
+  });
+
+  @override
+  bool operator ==(Object other) {
+    return runtimeType == other.runtimeType &&
+        other is AsyncUpdate<TData> &&
+        other.status == status &&
+        other.error == error &&
+        other.value == value;
+  }
+
+  @override
+  int get hashCode => Object.hash(
+        runtimeType,
+        status,
+        error,
+        value,
+      );
 }
 
-/// Status of a mutation
+class Idle<TData> extends AsyncUpdate<TData> {
+  const Idle() : super._();
+
+  @override
+  String toString() => 'AsyncUpdate<$TData>.idle()';
+
+  @override
+  R map<R>({
+    required R Function() idle,
+    required R Function(TData data) data,
+    required R Function(Object? error, StackTrace? stackTrace) error,
+    required R Function() loading,
+  }) {
+    return idle();
+  }
+}
+
+class Success<TData> extends AsyncUpdate<TData> {
+  const Success(this.value) : super._();
+
+  final TData value;
+
+  @override
+  String toString() => 'AsyncUpdate<$TData>.success($value)';
+
+  @override
+  R map<R>({
+    required R Function() idle,
+    required R Function(TData data) data,
+    required R Function(Object? error, StackTrace? stackTrace) error,
+    required R Function() loading,
+  }) {
+    return data(value);
+  }
+}
+
+class Loading<TData> extends AsyncUpdate<TData> {
+  const Loading() : super._();
+
+  @override
+  String toString() => 'AsyncUpdate<$TData>.loading()';
+
+  @override
+  R map<R>({
+    required R Function() idle,
+    required R Function(TData data) data,
+    required R Function(Object? error, StackTrace? stackTrace) error,
+    required R Function() loading,
+  }) {
+    return loading();
+  }
+}
+
+class ErrorDetails<TData> extends AsyncUpdate<TData> {
+  const ErrorDetails(this.message, this.stackTrace) : super._();
+
+  final Object? message;
+  final StackTrace? stackTrace;
+
+  @override
+  String toString() => 'AsyncUpdate<$TData>.error($message)';
+
+  @override
+  R map<R>({
+    required R Function() idle,
+    required R Function(TData data) data,
+    required R Function(Object? error, StackTrace? stackTrace) error,
+    required R Function() loading,
+  }) {
+    return error(message, stackTrace);
+  }
+}
+
 enum Status {
   /// Your mutation has never run, `mutate` hasn't been called yet.
   idle,
