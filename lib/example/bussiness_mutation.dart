740b40399e76ad2804487e954581485cd29045a0
matthias-prevost <matthias.prevost@gmail.com>
2025-05-22 09:26:34 +0200

    doc: simplify example

--- lib/example/bussiness_mutation.dart
+++ lib/example/bussiness_mutation.dart
@@ -4,40 +4,21 @@ import 'package:riverpod_community_mutation/src/async_update.dart';
 import 'package:riverpod_community_mutation/src/mutation.dart';
 import 'package:riverpod/riverpod.dart';
 
-// First, create your API or service class
-class TodoApi {
-  Future<Todo> createTodo(String title) async {
-    // Simulate API call
-    await Future.delayed(const Duration(seconds: 1));
-    return Todo(id: DateTime.now().millisecondsSinceEpoch, title: title);
-  }
-}
-
-// Create your data model
-class Todo {
-  const Todo({required this.id, required this.title});
-  final int id;
-  final String title;
-}
-
-class CreateTodoMutation extends AutoDisposeNotifier<AsyncUpdate<Todo>>
+class CreateTodoMutation extends AutoDisposeNotifier<AsyncUpdate<void>>
     with Mutation {
   @override
-  AsyncUpdate<Todo> build() {
-    return const AsyncUpdate<Todo>.idle();
+  AsyncUpdate<void> build() {
+    return const AsyncUpdate<void>.idle();
   }
 
   Future<void> createTodo(
     String title, {
-    Future<void> Function(Todo)? onSuccess,
+    Future<void> Function(void)? onSuccess,
     Future<void> Function(Object? error)? onError,
   }) {
     return mutate(
       () async {
-        final todoApi = ref.read(todoApiProvider);
-
-        final todo = await todoApi.createTodo(title);
-        return todo;
+        await Future.delayed(const Duration(seconds: 1));
       },
       onSuccess: onSuccess,
       onError: onError,
@@ -45,10 +26,7 @@ class CreateTodoMutation extends AutoDisposeNotifier<AsyncUpdate<Todo>>
   }
 }
 
-// Create your API provider
-final todoApiProvider = Provider((ref) => TodoApi());
-
 final bussinessMutationProvider =
-    AutoDisposeNotifierProvider<CreateTodoMutation, AsyncUpdate<Todo>>(
+    AutoDisposeNotifierProvider<CreateTodoMutation, AsyncUpdate<void>>(
   CreateTodoMutation.new,
 );
